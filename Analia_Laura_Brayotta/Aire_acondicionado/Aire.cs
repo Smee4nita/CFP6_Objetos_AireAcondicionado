1a16d42144ecf09ad13e57cb6d36cace1cc39c8b
Smee4nita <b.analia.laura@gmail.com>
2024-05-12 11:35:35 -0300

    creacion de los aires

---
+++ Analia_Laura_Brayotta/Aire_acondicionado/Aire.cs
@@ -0,0 +1,27 @@
+﻿using Linreria_Aires_Acondicionados;
+namespace Aire_acondicionado
+{
+    internal class Aire
+    {
+        static void Main(string[] args)
+        {
+            AireAcondicionado aireUno = new AireAcondicionado("ventana", "sanyo", 3000, 00);
+
+            AireAcondicionado aireDos = new AireAcondicionado("ventana", "LG", 4500, 00);
+
+            AireAcondicionado aireTres = new AireAcondicionado("split", "Midea", 3000, 00);
+
+            AireAcondicionado aireCuatro = new AireAcondicionado("split inverter", "sanyo", 3000, 00);
+
+            AireAcondicionado aireCinco = new AireAcondicionado("ventana", "hisense", 3500, 00);
+
+            AireAcondicionado aireSeis = new AireAcondicionado("portatil", "surrey", 3800, 00);
+
+            List<AireAcondicionado> ListaAires = new List<AireAcondicionado>();
+            ListaAires.Add(aireUno);
+            ListaAires.Add(aireDos);
+            ListaAires.Add(aireTres);
+            
+        }
+    }
+}
---
+++ Analia_Laura_Brayotta/Linreria_Aires_Acondicionados/Aire_Acondicionado.cs
@@ -0,0 +1,72 @@
+﻿using System.Drawing;
+using System.Reflection;
+
+namespace Linreria_Aires_Acondicionados
+{
+    public class AireAcondicionado
+    {
+        private string modelo;
+        private string marca;
+        private int watts;
+        private Color color;
+        private double temperatura;
+     
+        public AireAcondicionado(string modelo, string marca, int watts, Color color, double temperatura)
+        {
+            this.modelo=modelo; 
+            this.marca= marca;
+            this.watts=watts;
+            this.color=color;
+            this.temperatura=0;
+        }
+
+        public AireAcondicionado(string modelo, string marca, int watts, double temperatura)
+        {
+            this.modelo=modelo;
+            this.marca= marca;
+            this.watts=watts;
+            this.temperatura=0;
+        }
+
+        public string GetMoldelo()
+        {
+            return modelo;
+        }
+        public int GetWatts()
+        {
+            return watts;
+        }
+        public string GetMarca()
+        {
+            return marca;
+        }
+
+        public Color GetColor()
+        {
+            return color;
+        }
+
+        public double GetTemperatura() 
+        {
+            return temperatura;
+        }
+
+        public void SetColor( Color color)
+        {
+            this.color = color;
+        }
+        public void SetTemperatura(double temperatura)
+        {
+            this.temperatura=temperatura;
+        }
+
+
+
+        public string ElectrodomesticoToString()
+        {
+            return $"Electrodomestico: Modelo: {modelo} - Marca: {marca} - Watts: {watts}  - Color: {color} -Temperatura {temperatura} ";
+
+        }
+   
+    }
+}
